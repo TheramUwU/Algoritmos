3e3f9e01d1c9ff1eee4ae865583743e7c367a6c3
TheramxD <ramsesdelgadillo61@aragon.unam.mx>
2023-08-31 19:05:39 -0600

    init

--- menor.js
+++ menor.js
@@ -2,10 +2,12 @@ var numeros = [];
 
 function menor(numeros){
     var menor = numeros[0];
+    var index = 0;
     for (let i = 1; i < numeros.length; i++) {
         if(menor < numeros[i]) {
             menor = numeros[i];
+            index = i;
         }
     }
-    return menor;
+    return index;
 }
