56eb8992909c15010bafb1e333fb5f1e9b062964
TheramUwU <69657583+TheramUwU@users.noreply.github.com>
2023-09-19 21:00:35 -0600

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -2,7 +2,7 @@ var numero;
 
 function factorial(numero) {
     var resultado = 1;
-    for (let i = 2; i < numero; i++) {
+    for (let i = 2; i <= numero; i++) {
         resultado = resultado * i;    
     }
     return resultado;
